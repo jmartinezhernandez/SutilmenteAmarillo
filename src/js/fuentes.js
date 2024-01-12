82f74061c7fb74049103ec656fbbb4a70c823107
Jesus <jmartinezhernandez1@uoc.edu>
2024-01-12 01:36:29 +0100

    Mejorado el rendimiento en jefeWiggum y corregido script de fuentes

---
+++ src/js/fuentes.js
@@ -0,0 +1,9 @@
+const linkMontserrat = document.createElement('link');
+linkMontserrat.rel = 'stylesheet';
+linkMontserrat.href = 'https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap';
+document.head.appendChild(linkMontserrat);
+
+const linkRockSaltSans = document.createElement('link');
+linkRockSaltSans.rel = 'stylesheet';
+linkRockSaltSans.href = 'https://fonts.googleapis.com/css2?family=Rock+Salt&family=Source+Sans+3:ital,wght@0,400;0,700;1,400;1,700&display=swap';
+document.head.appendChild(linkRockSaltSans);
